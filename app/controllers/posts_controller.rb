3b30f37bfd7029ecdc78e1322a6787dc81592742
Yazima <yazima04200@gmail.com>
2023-03-04 23:56:56 +0900

    createにエラー表示を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,22 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(created_at: :desc)
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.image = params[:post][:image] # 追加するコード
+    if @post.save
+      redirect_to @post, notice: '投稿が作成されました。'
+    else
+      flash.now[:alert] = @post.errors.full_messages.join(',有効な画像ファイル(.jpg, .png, .gif)を選択してください')
+      render :new
+    end
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+  
 end
